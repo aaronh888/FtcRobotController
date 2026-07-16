735c8cfc503d3c3d0ec76f9c5c4497a07b8d9d9c
Aaron <36175890+aaronh888@users.noreply.github.com>
2026-07-16 15:11:55 -0700

    Updates as pushed by the software

---
+++ TeamCode/src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp
+public class HelloWorld extends OpMode {
+
+    @Override
+    public void init() {
+        telemetry.addData(caption:"Hello", v)
+    }
+}
